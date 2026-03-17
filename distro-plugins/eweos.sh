338f119f54c5470dd6786c1c1227995252808207
怪异软糖 <219617512+WeirdGumi@users.noreply.github.com>
2026-03-17 23:06:53 +0800

    Update eweos.sh

--- distro-plugins/eweos.sh
+++ distro-plugins/eweos.sh
@@ -3,9 +3,9 @@
 # If you want customize installation, please make a copy.
 DISTRO_NAME="eweOS"
 
-TARBALL_URL['aarch64']="file:///storage/emulated/0/Download/eweos-aarch64-pd-v4.38.0.tar.xz"
+TARBALL_URL['aarch64']="file://$HOME/eweos-aarch64-pd-v4.38.0.tar.xz"
 TARBALL_SHA256['aarch64']="7118aca52ae2cd24deda7bd9b167809d07e489b8719559077616915feead66d7"
-TARBALL_URL['riscv64']="file:///storage/emulated/0/Download/eweos-riscv64-pd-v4.38.0.tar.xz"
+TARBALL_URL['riscv64']="file://$HOME/eweos-riscv64-pd-v4.38.0.tar.xz"
 TARBALL_SHA256['riscv64']="cdcfbacd8432f2321231056ef374b036db4dd608220d6f5e3a4235dc75ae2ba9"
-TARBALL_URL['x86_64']="file:///storage/emulated/0/Download/eweos-x86_64-pd-v4.38.0.tar.xz"
+TARBALL_URL['x86_64']="file://$HOME/eweos-x86_64-pd-v4.38.0.tar.xz"
 TARBALL_SHA256['x86_64']="6b4dd5dbd0211e1bea2ee61cc02cd35adc1b04f397289076503a905155d691ab"
